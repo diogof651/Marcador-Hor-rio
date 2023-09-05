885025361b08357e0153e5031b186f9e16de5455
Diogo Felipe <67284614+diogof651@users.noreply.github.com>
2023-09-05 16:51:00 -0400

    Adição da Marcação Compilação.

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         const selectedCompanies = Array.from(document.querySelectorAll('input[name="company"]:checked')).map(checkbox => checkbox.value);
         const selectedCommunication = Array.from(document.querySelectorAll('input[name="communication"]:checked')).map(checkbox => checkbox.value);
+        const isCompilation = document.getElementById("compilation").checked;
         const user = document.getElementById("user").value;
         const description = document.getElementById("description").value;
 
@@ -14,21 +15,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
         selectedCompanies.forEach(company => {
             selectedCommunication.forEach(communication => {
-                if ((company === "Lontano" || company === "Copas" || company === "Agrogrande") && communication !== "E-mail" && communication !== "WhatsApp") {
-                    const match = description.match(/^#(\d{5})/);
-                    const ticket = match ? `#${match[1]}` : "";
+                if (isCompilation) {
+                    let compilationMessage = "Compilação e atualização do projeto";
+                    const projects = description.split(',').map(project => project.trim());
+                    if (projects.length > 1) {
+                        compilationMessage += "s";
+                    }
+                    compilationMessage += ": " + projects.join(", ") + ".";
+                    result += compilationMessage;
+                } else {
+                    if ((company === "Lontano" || company === "Copas") && communication !== "E-mail" && communication !== "WhatsApp") {
+                        const match = description.match(/^#(\d{5})/);
+                        const ticket = match ? `#${match[1]}` : "";
 
-                    const descriptionWithoutPrefix = description.split('-')[1].trim();
+                        const descriptionWithoutPrefix = description.split('-')[1].trim();
 
-                    result += `Atendimento a usuário(a): ${user} com chamado no ${communication} ${ticket ? ticket + ' ' : ''}sobre: ${descriptionWithoutPrefix}\n`;
-                } else if (company === "Lontano" || company === "Copas" || company === "Agrogrande") {
-                    const descriptionWithoutPrefix = description.split('-')[1].trim();
-                    result += `Atendimento a usuário(a): ${user} com chamado no ${communication} sobre: ${descriptionWithoutPrefix}\n`;
-                } else if (company === "Rodomaior" || company === "Mademaior") {
-                    if (communication === "E-mail") {
-                        result += `Atendimento a usuário(a): ${user} com e-mail encaminhado a GS sobre: ${description}\n`;
-                    } else {
-                        result += `Atendimento a usuário(a): ${user} com chamado no grupo do WhatsApp sobre: ${description}\n`;
+                        result += `Atendimento a usuário(a): ${user} com chamado no ${communication} ${ticket ? ticket + ' ' : ''}sobre: ${descriptionWithoutPrefix}\n`;
+                    } else if (company === "Lontano" || company === "Copas") {
+                        const descriptionWithoutPrefix = description.split('-')[1].trim();
+                        result += `Atendimento a usuário(a): ${user} com chamado no ${communication} sobre: ${descriptionWithoutPrefix}\n`;
+                    } else if (company === "Rodomaior" || company === "Mademaior") {
+                        if (communication === "E-mail") {
+                            result += `Atendimento a usuário(a): ${user} com e-mail encaminhado a GS sobre: ${description}\n`;
+                        } else {
+                            result += `Atendimento a usuário(a): ${user} com chamado no grupo do WhatsApp sobre: ${description}\n`;
+                        }
                     }
                 }
             });
